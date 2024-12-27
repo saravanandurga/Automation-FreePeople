8b8deb148991638cc79276f5d90e22e76c2ece60
saravanandurga <saravanandurga333@gmail.com>
2024-12-27 20:11:31 +0530

    Added Methods

--- src/test/java/baseclass/BaseClass.java
+++ src/test/java/baseclass/BaseClass.java
@@ -15,6 +15,21 @@ public class BaseClass {
 		System.out.println("3");
 	}
 	
+	private void tamilNadu() {
+		
+		System.out.println("tamil");
+	}
+	
+	private void andra() {
+		
+		System.out.println("telugu");
+	}
+	
+	private void kerala() {
+		
+		System.out.println("malayalam");
+	}
+	
 	
 	
 }
